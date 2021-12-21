8317ac1b2cb7b19b4254bef076d711e180a379bf
gojali99 <fajarsidik1999@gmail.com>
2021-12-21 14:13:37 +0700

    add responsive layout

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,67 +7,79 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double ScreenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'VIGENERE CIPHER APP',
-            style: TextStyle(fontSize: 35),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Image.asset(
-            'assets/logo.png',
-            width: 150,
-            height: 150,
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: GridView.count(
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 2,
-              crossAxisCount: 2,
-              shrinkWrap: true,
-              primary: false,
-              children: <Widget>[
-                InkWell(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return Screen(
-                        title: 'VIGENERE CIPHER',
-                      );
-                    }));
-                  },
-                  child: Card(
-                    child: Container(
-                      margin: const EdgeInsets.all(15),
-                      child: Image.asset(
-                        'assets/start.png',
-                      ),
-                    ),
-                  ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            width: ScreenWidth > 650
+                ? 600
+                : ScreenWidth > 900
+                    ? 200
+                    : 500,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'VIGENERE CIPHER APP',
+                  style: TextStyle(fontSize: 35),
                 ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const About();
-                    }));
-                  },
-                  child: Card(
-                    child: Image.asset('assets/about.jpg'),
+                const SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  'assets/logo.png',
+                  width: 150,
+                  height: 150,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: GridView.count(
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 2,
+                    crossAxisCount: 2,
+                    shrinkWrap: true,
+                    primary: false,
+                    children: <Widget>[
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return Screen(
+                              title: 'VIGENERE CIPHER',
+                            );
+                          }));
+                        },
+                        child: Card(
+                          child: Container(
+                            margin: const EdgeInsets.all(15),
+                            child: Image.asset(
+                              'assets/start.png',
+                            ),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return const About();
+                          }));
+                        },
+                        child: Card(
+                          child: Image.asset('assets/about.jpg'),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
